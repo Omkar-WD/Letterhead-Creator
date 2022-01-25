1121808b453d20440e0a1937f03de81c40563195
Omkar-WD <omkarpasalkar2020@gmail.com>
2022-01-25 22:33:30 +0530

    create sample header , add btn to download the webpage as pdf with functionality

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function () {
+  document.querySelector("#header_left_div>img").src =
+    "https://via.placeholder.com/250";
+  document.querySelector("#header_right_div_address_line1>h3").textContent =
+    "Flat No - 505" + " ,";
+  document.querySelector("#header_right_div_apartment>h3").textContent =
+    "Gajanan Sankul Society" + " ,";
+  document.querySelector("#header_right_div_city>h3").textContent =
+    "Thane" + " ,";
+  document.querySelector("#header_right_div_state>h3").textContent =
+    "Maharashtra" + " ,";
+  document.querySelector("#header_right_div_country>h3").textContent =
+    "India" + " ,";
+  document.querySelector("#header_right_div_zipcode>h3").textContent =
+    "421302" + ".";
+});
